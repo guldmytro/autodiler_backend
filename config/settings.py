47d7a1216668dcbbdd834a1ef9baffea1873b02d
Dmytro <guldmytro@gmail.com>
2025-06-13 14:11:16 +0300

    added monobank

--- config/settings.py
+++ config/settings.py
@@ -271,7 +271,7 @@ except ImportError:
     # LiqPay
     LIQPAY_PUBLIC_KEY = 'some'
     LIQPAY_PRIVATE_KEY = 'key'
-    MONOBANK_TOKEN = 'mnXx38bK9hYn3hfY0_DLXqQ'
+    MONOBANK_TOKEN = 'key'
 
 # database trigram
 # sudo -i -u postgres
